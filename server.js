f544d1597a3cf98d0db2a59c40cff670b333b99b
spark72018 <spark89@gmail.com>
2017-03-27 13:57:15 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const apiKey = process.env.MAPKEY;
+const googleMapsClient = require('@google/maps').createClient({
+  key: apiKey
+});
+console.log(apiKey);
+
+app.get('/favicon.ico', (req, res) { //disable favicon
+  res.sendStatus(204);
+}));
+
+app.get('/', (req, res) => {
+  googleMapsClient.geocode({
+    address: '1600 Amphitheatre Parkway, Mountain View, CA'
+  }, function(err, res) {
+    if(!err) {
+      res.send(response.json.results);
+    }else {
+      console.log('error occurred: ', err);
+    }
+  });
+});
+
+app.listen(process.env.PORT || 3000);
