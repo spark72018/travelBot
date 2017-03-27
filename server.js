d56d4c77a27e22e200f4802e35b74f4c9d12dfa8
spark72018 <spark89@gmail.com>
2017-03-27 15:16:44 -0400

    testing out promise on geocode method

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const urlencodedParser = bodyParser.urlencoded({extended: false});
-const app = express();
-const apiKey = process.env.MAPKEY;
-const googleMapsClient = require('@google/maps').createClient({
+const express = require('express'),
+      bodyParser = require('body-parser'),
+      urlencodedParser = bodyParser.urlencoded({extended: false}),
+      app = express(),
+      apiKey = process.env.MAPKEY,
+      slackToken = process.env.SLACKTOKEN,
+      googleMapsClient = require('@google/maps').createClient({
   key: apiKey
+}),
+
+const validator = (() => {
+  let isEqual = (p1, p2) => p1 === p2;
+
+  return {
+    isEqual: isEqual
+  };
+})();
+
+/*
+app.post('/slack/map', urlencodedParser, (req, res) => {
+
 });
+*/
 
 app.get('/favicon.ico', (req, res) => { //disable favicon
   res.sendStatus(204);
 });
 
-app.get('/',urlencodedParser ,(req, res) => {
-  googleMapsClient.geocode({
-    address: '1600 Amphitheatre Parkway, Mountain View, CA'
-  }, function(err, res) {
-    if(!err) {
-      console.log(res.json.results);
-      res.send(res.json.results);
-    }else {
-      console.log('error occurred: ', err);
-    }
+app.get('/', (req, res) => {
+  let mapPromise = new Promise((resolve, reject) => {
+    googleMapsClient.geocode({
+      address: '1600 Amphitheatre Parkway, Mountain View, CA'
+    }, (err, response) {
+      if(!err) {
+        console.log(response.json.results);
+        res.send(response.json.results);
+      }else {
+        console.log('error occurred, ', err);
+      }
+    });
   });
 });
 
