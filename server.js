08b6554e1d77c6a7be780c0e65444f0ba8094718
spark72018 <spark89@gmail.com>
2017-04-01 14:22:41 -0400

    deleted unnecessary comments from previous slackTest and added comments to explain app.post body

--- server.js
+++ server.js
@@ -9,100 +9,6 @@ var express = require('express'),
       Promise: Promise
     });
 
-/*
-//https://tranquil-harbor-42592.herokuapp.com/
-var isEqual = function(p1, p2) {
-    return p1 === p2;
-};
-
-var replacer = function(str, obj) {
-  var replacerStr = str,
-      regex,
-      expression;
-  for(expression in obj) {
-      regex = new RegExp(expression, 'g');
-      replacerStr = replacerStr.replace(regex, obj[expression]);
-  }
-  return replacerStr;
-};
-
-app.get('/', function(req, res) {
-    console.log('success!');
-    res.send('success!');
-});
-
-app.post('/slack/hello', urlencodedParser, function(req, res) {
-    if(isEqual(req.body.token, slackEnvToken)) {
-        res.send({
-            'response_type': 'in_channel',
-            'text': "Hello there!"
-        });
-    }else {
-        res.sendStatus(500);
-    }
-});
-
-app.post('/slack/inspire', urlencodedParser, function(req, res) {
-    //get random quote and send if token matches
-    var myPromise = new Promise(function(resolve, reject) {
-        http.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=' +
-        new Date().getTime(), function(res) {
-            var statusCode = res.statusCode;
-            if(statusCode != 200) {
-                console.log('did not get quote!');
-                return -1;
-            }
-            res.setEncoding('utf-8');
-            var rawData = '';
-            res.on('data', function(chunk) {
-                rawData += chunk;
-            });
-            res.on('end', function() {
-                var parsedData = JSON.parse(rawData);
-                resolve(parsedData);
-            });
-        });
-    });
-    myPromise.then(function(quoteData) {
-        if(isEqual(req.body.token, slackEnvToken)) {
-            var formattedQuote = replacer(quoteData[0].content, {
-                '<p>|<\/p>': '',
-                '&#8217;': '\'',
-                '&#8220;|&#8221': '\"',
-                '&#038;': '&',
-                '&#8230;': '...'
-            });
-            res.send({
-                'response_type': 'in_channel',
-                'text': formattedQuote + ' -' + quoteData[0].title
-            });
-        }else {
-            console.log('isEqual for quoteData failed');
-            res.sendStatus(500);
-        }
-    });
-});
-*/
-
-/*
-app.get('/', function(req, res) {
-  googleMapsClient.geocode({
-    address: '1600 Amphitheatre Parkway, Mountain View, CA'
-  }, function(err, response) {
-    if(!err) {
-      console.log(response.json.results);
-      //res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-      res.send(response.json.results);
-    }else {
-      console.log(err);
-    }
-  });
-});
-
-app.listen(process.env.PORT || 3000);
-*/
-
-
 //Start of if block (serves static image when user enters 1 input)
 /*
 app.get('/', function(req, res) {
@@ -129,31 +35,37 @@ app.get('/', function(req, res) {
       });
     }
   } else {Steve's part}
-}  
+}
 */
 
+
 /*
-//we can input appropriate responses, will return response object
-
-//also, if we want to send error text, just put our error message
-//into textInput param
-
-//attachmentInput is an array of objects, debating whether to write
-//argument validation in function body, leaning towards not
+-generates our response object
+-example usage:
+var ourBot = botStore("THIS\nIS\nSPARTA", [{image_url: 'some url link'}]);
+console.log(ourBot.response()); //will output Object
+                                              attachments: Array[1]
+                                              text: "THIS↵IS↵SPARTA"
+                                              __proto__: Object
+*/
+//needs work, code is not DRY
 var botStore = function(textInput, attachmentInput) {
   var store,
       argsLength = arguments.length;
   if(argsLength === 2) {
     store = {
+      'response_type': 'in_channel',
       'text': arguments[0],
       'attachments': arguments[1]
     };
   }else if(typeof arguments[0] === 'string') {
     store = {
+      'response_type': 'in_channel',
       'text': arguments[0]
     };
   }else {
     store = {
+      'response_type': 'in_channel',
       'attachments' : arguments[0]
     };
   }
@@ -164,44 +76,66 @@ var botStore = function(textInput, attachmentInput) {
   };
 };
 
-example usage:
-var ourBot = botStore("THIS\nIS\nSPARTA", [{image_url: 'some url link'}]);
-console.log(ourBot.response()); //will output Object
-                                              attachments: Array[1]
-                                              text: "THIS↵IS↵SPARTA"
-                                              __proto__: Object
-*/
+//need to add token validation
+  //what is difference between regular slack token validation and OAuth validation???
+//need to add valerie's address validation
+//'/directions' just for sake of testing, can change to better command
+app.post('/directions', urlencodedParser, function(req, res) {
+  var splitted = req.body.text.split('>'),
+      splittedLength = splitted.length,
+      directionsString = '',
+      start, //starting geocode
+      finish; //finishing geocode
 
-/*initial attempt at getting user input and doing stuff with it
-app.get('/someCommand', function(req, res) {
-  //cache appropriate lookups here?
-    //valerie's if block code here
-    //steve's  else code here
-      //googleMapsClient.geocode code here, then
-      //process geocode with googleMapsClient.directions code here
-        //cache response.routes[0].legs into variable (one time object lookup)
-        //response.routes[0].legs.steps --> array containing actual route steps
-          //figure out how to process google's directions response, use my
-          //replacer function from slack quote command (not sure)
-  //var ourBot = botStore(directions, image);
-  //var ourResponse = ourBot.response();
-  //res.send(ourResponse);
+//valerie's if block first (static map)
+//steve's else block second (directions)
+// '/dirMap' for integrated response?
 
-});
+  //timestamp console.log to easily discern app startup in heroku logs
+  console.log(new Date().toLocaleString());
 
-*/
+  //to see what user response gets split to
+  console.log('splitted is ', splitted);
 
-app.get('/', function(req, res) {
-  googleMapsClient.directions({
-    origin: { lat: 40.7720280, lng: -73.4974010 },
-    destination: {lat: 40.8425820, lng: -73.7171510 }
-  }).asPromise()
-  .then(function(response) {
-    console.log(response.json);
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.send(response.json);
+  //this will go in else block
+  var p1 = googleMapsClient.geocode({
+    address: splitted[0]
+  }).asPromise();
+
+  var p2 = googleMapsClient.geocode({
+    address: splitted[1]
+  }).asPromise();
+
+  //this promise will wait for p1 and p2 to resolve before resolving itself
+  var p3 = Promise.all([p1, p2])
+
+  .then(function(values) {
+    start = values[0].json.results[0].geometry.location;
+    finish = values[1].json.results[0].geometry.location;
+
+    googleMapsClient.directions({
+      origin: start,
+      destination: finish
+    }).asPromise()
+
+    .then(function(result) {
+      //route is an array where each element is an object containing one line
+      //of the route in the .html_instructions property
+      var route = result.json.routes[0].legs[0].steps,
+          resultString = '';
+
+      //need to format last line properly after </div>
+      route.forEach(function(el) {
+        resultString += el.html_instructions.replace(/<b>|<\/b>|<div (.*?)>|<\/div>/g, '') + '\n';
+      });
+      //console.log('resultString is', resultString);
+      var ourBot = botStore(resultString);
+      var theResponse = ourBot.response();
+      console.log(theResponse);
+      res.send(theResponse);
+    });
   });
-
 });
+
 
 app.listen(process.env.PORT || 3000);
