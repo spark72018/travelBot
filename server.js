453ca71e3baeb7b70b52ccb06c7ad4bd14e47fb9
valerieseer <seervalerie@gmail.com>
2017-04-10 21:43:05 -0500

    added foundation for auth; added a help command; cleaned up code

--- server.js
+++ server.js
@@ -12,39 +12,6 @@ var express = require('express'),
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-//Start of if block (serves static image when user enters 1 input)
-
-/*
-app.get('/', function(req, res) {
-  var cmd = req.body.text;
-  if (cmd.split(">").length === 1) {
-    if (cmd.test("\\d+\\s+([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+")) {
-      var formattedInput = cmd.replace(\/s\g, '+');
-      var url = "https://maps.googleapis.com/maps/api/staticmap?center="+formattedInput+"&size=600x400&markers="+formattedInput;
-
-      res.send({
-        response_type: 'in_channel',
-        "title": cmd,
-        "title_link": url,
-        attachments:[
-          {
-            image_url: url;
-          }
-        ]
-      });
-    } else {
-      res.send({
-        response_type: 'in_channel',
-        "text": "Enter a valid address!",
-      });
-    }
-  } else {
-
-  }
-}
-*/
-
-
 /*
 -generates our response object
 -example usage:
@@ -55,6 +22,7 @@ console.log(ourBot.response()); //will output Object
                                               __proto__: Object
 */
 //needs work, code is not DRY
+
 var botStore = function(textInput, attachmentInput) {
   var store,
       argsLength = arguments.length;
@@ -82,151 +50,124 @@ var botStore = function(textInput, attachmentInput) {
   };
 };
 
-//mapMe mapMeDrive mapMeBike mapMeWalk mapMeWhatever
-  //include ETA for all of them
-app.post('/directions', function(req, res) {
-  /*
-  var splitted = req.body.text.split('>'),
-      splittedLength = splitted.length,
-      directionsString = '',
-      start, //starting geocode
-      finish; //finishing geocode
-*/
-//valerie's if block first (static map)
-//steve's else block second (directions)
-// '/dirMap' for integrated response?
-var input = req.body.text;
-var splitted = input.split(">");
-var regex = /\d+\s+([a-zA-Z]+|[a-zA-Z]+\s[a-zA-Z]+)/g;
-console.log('outside if/else, splitted is ', splitted);
-if (regex.test(input)) {
-  if(splitted.length === 1) {
-    var formattedInput = input.replace(/\s/g, '+');
-    console.log('this is formattedInput ' + formattedInput);
-    var url = "https://maps.googleapis.com/maps/api/staticmap?center="+formattedInput+"&size=600x400&markers="+formattedInput;
+//Auth
+app.get('/slack', function(req, res){
+  var data = {form: {
+      client_id: process.env.SLACK_CLIENT_ID,
+      client_secret: process.env.SLACK_CLIENT_SECRET,
+      code: req.query.code
+  }};
+  request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // Get an auth token
+      let token = JSON.parse(body).access_token;
 
-    res.send({
-      response_type: 'in_channel',
-      attachments:[
-        {
-          "title": input,
-          "title_link": "http://maps.google.com/maps?f=d&source=s_d&saddr=&daddr="+formattedInput,
-          image_url: url
+      // Get the team domain name to redirect to the team URL after auth
+      request.post('https://slack.com/api/team.info', {form: {token: token}}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          if(JSON.parse(body).error == 'missing_scope') {
+            res.send('MapMe has been added to your team!');
+          } else {
+            let team = JSON.parse(body).team.domain;
+            res.redirect('http://' +team+ '.slack.com');
+          }
         }
-      ]
-    });
-  }else if (splitted.length === 2) {
-    //timestamp console.log to easily discern app startup in heroku logs
-    console.log(new Date().toLocaleString());
-
-    //to see what user response gets split to
-    console.log('splitted is ', splitted);
-
-    //this will go in else block
-    var p1 = googleMapsClient.geocode({
-      address: splitted[0]
-    }).asPromise();
-
-    var p2 = googleMapsClient.geocode({
-      address: splitted[1]
-    }).asPromise();
-
-    //this promise will wait for p1 and p2 to resolve before resolving itself
-    var p3 = Promise.all([p1, p2])
-
-    .then(function(values) {
-      start = values[0].json.results[0].geometry.location;
-      finish = values[1].json.results[0].geometry.location;
-
-      googleMapsClient.directions({
-        origin: start,
-        destination: finish
-      }).asPromise()
-
-      .then(function(result) {
-        //route is an array where each element is an object containing one line
-        //of the route in the .html_instructions property
-        var route = result.json.routes[0].legs[0].steps,
-            resultString = '';
-
-        //need to format last line properly after </div>
-        route.forEach(function(el) {
-          resultString += el.html_instructions.replace(/<b>|<\/b>|<div (.*?)>|<\/div>/g, '') + '\n';
-        });
-        //console.log('resultString is', resultString);
-        var ourBot = botStore(resultString);
-        var theResponse = ourBot.response();
-        console.log(theResponse);
-        res.send(theResponse);
       });
-    });
-  }
-}else {
-  res.send({
-    response_type: 'in_channel',
-    "text": "Enter a valid address!",
-  });
-}
-  /*
-  if (cmd.test("\\d+\\s+([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+")) {
-  } else {
-    res.send({
-      response_type: 'in_channel',
-      "text": "Enter a valid address!",
-    });
-  }
-} else {
-
-}
-*/
-
-/*
-  //timestamp console.log to easily discern app startup in heroku logs
-  console.log(new Date().toLocaleString());
-
-  //to see what user response gets split to
-  console.log('splitted is ', splitted);
-
-  //this will go in else block
-  var p1 = googleMapsClient.geocode({
-    address: splitted[0]
-  }).asPromise();
-
-  var p2 = googleMapsClient.geocode({
-    address: splitted[1]
-  }).asPromise();
-
-  //this promise will wait for p1 and p2 to resolve before resolving itself
-  var p3 = Promise.all([p1, p2])
-
-  .then(function(values) {
-    start = values[0].json.results[0].geometry.location;
-    finish = values[1].json.results[0].geometry.location;
-
-    googleMapsClient.directions({
-      origin: start,
-      destination: finish
-    }).asPromise()
-
-    .then(function(result) {
-      //route is an array where each element is an object containing one line
-      //of the route in the .html_instructions property
-      var route = result.json.routes[0].legs[0].steps,
-          resultString = '';
-
-      //need to format last line properly after </div>
-      route.forEach(function(el) {
-        resultString += el.html_instructions.replace(/<b>|<\/b>|<div (.*?)>|<\/div>/g, '') + '\n';
-      });
-      //console.log('resultString is', resultString);
-      var ourBot = botStore(resultString);
-      var theResponse = ourBot.response();
-      console.log(theResponse);
-      res.send(theResponse);
-    });
-    */
-  //});
+    }
+  })
 });
 
 
-app.listen(process.env.PORT || 3000);
+//Slash Commands
+app.post('/', function(req, res) {
+  var input = req.body.text;
+  var splitted = input.split(">");
+  var regex = /\d+\s+([a-zA-Z]+|[a-zA-Z]+\s[a-zA-Z]+)/g;
+  console.log('splitted = ', splitted);
+
+  //Help command
+  if (input === "help") {
+    res.send({
+      response_type: 'in_channel',
+      "text": "Get your directions by entering 2 addresses seperated with a >. Example,'123 N Main St > 456 S Main St.'" 
+    });
+  }
+
+  //Static img if 1 address input
+  if (regex.test(input)) {
+    if(splitted.length === 1) {
+      var formattedInput = input.replace(/\s/g, '+');
+      console.log('formattedInput = ' + formattedInput);
+      var url = "https://maps.googleapis.com/maps/api/staticmap?center="+formattedInput+"&size=600x400&markers="+formattedInput;
+
+      res.send({
+        response_type: 'in_channel',
+        attachments:[
+          {
+            "title": input,
+            "title_link": "http://maps.google.com/maps?f=d&source=s_d&saddr=&daddr="+formattedInput,
+            image_url: url
+          }
+        ]
+      }); //Directions else if 2 address inputs
+    } else if (splitted.length === 2) { 
+      //timestamp console.log to easily discern app startup in heroku logs
+      console.log(new Date().toLocaleString());
+
+      //to see what user response gets split to
+      console.log('splitted is ', splitted);
+
+      //this will go in else block
+      var p1 = googleMapsClient.geocode({
+        address: splitted[0]
+      }).asPromise();
+
+      var p2 = googleMapsClient.geocode({
+        address: splitted[1]
+      }).asPromise();
+
+      //this promise will wait for p1 and p2 to resolve before resolving itself
+      var p3 = Promise.all([p1, p2])
+
+      .then(function(values) {
+        start = values[0].json.results[0].geometry.location;
+        finish = values[1].json.results[0].geometry.location;
+
+        googleMapsClient.directions({
+          origin: start,
+          destination: finish
+        }).asPromise()
+
+        .then(function(result) {
+          //route is an array where each element is an object containing one line
+          //of the route in the .html_instructions property
+          var route = result.json.routes[0].legs[0].steps,
+              resultString = '';
+
+          //need to format last line properly after </div>
+          route.forEach(function(el) {
+            resultString += el.html_instructions.replace(/<b>|<\/b>|<div (.*?)>|<\/div>/g, '') + '\n';
+          });
+          //console.log('resultString is', resultString);
+          var ourBot = botStore(resultString);
+          var theResponse = ourBot.response();
+          console.log(theResponse);
+          res.send(theResponse);
+        });
+      });
+    }
+  } else {
+      res.send({
+        response_type: 'in_channel',
+        "text": "Enter a valid address!",
+      });
+  }
+});
+
+
+//Listening
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Listening on port " + port + "!");
+});
