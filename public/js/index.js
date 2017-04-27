eb0374a4e019cba6ccfaba5bc7bd6e9b04108ce7
valerieseer <seervalerie@gmail.com>
2017-04-26 20:50:50 -0500

    animated background on scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,27 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
 
 	// hide back-to-top first
 	$(".back-to-top").hide();
 	
-	// fade in back-to-top
-	$(function () {
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 200) {
-				$('.back-to-top').fadeIn(1000);
-			} else {
-				$('.back-to-top').fadeOut();
-			}
-		});
-
-		// scroll body to 0px on click
-		$('.back-to-top a').click(function () {
-			$('body, html').animate({
-				scrollTop: 0
-			}, 700);
-			return false;
-		});
+	// fade in back-to-top, background position y
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 200) {
+			$('.back-to-top').fadeIn(1000);
+		} else {
+			$('.back-to-top').fadeOut();
+		}
+		//Animate background on scroll
+		$('body').css("background-position-y", parseInt($(this).scrollTop()*0.1));
 	});
 
-	// $('.main-section').hide();
+	// scroll body to 0px on click
+	$('.back-to-top a').click(function () {
+		$('body, html').animate({
+			scrollTop: 0
+		}, 700);
+		return false;
+	});
 
 });
