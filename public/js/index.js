fc20bb5a10fe5e35a6a241fcaff4420189e9a1cd
Valerie Seer <seervalerie@gmail.com>
2017-04-30 11:30:35 -0400

    landing height; back to top right

--- public/js/index.js
+++ public/js/index.js
@@ -1,39 +1,43 @@
 $(document).ready(function() {
+	//Hide back-to-top btn at first
+	$(".back-to-top").hide();
+
+	//Scroll to top of the document when click on back to top btn
+	$(".back-to-top a").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 700);
+		return false;
+	});
+
+
+	//Fn to fade in divs on scroll
 	function fadeDiv() {
-        $('.main-section').each(function() {
+        $(".main-section").each(function() {
             var bottomOfObject = $(this).position().top;
             var bottomOfWindow = $(window).scrollTop() + $(window).height();
             
             //Fade in div
             if (bottomOfWindow > bottomOfObject) {
-                $(this).animate({'opacity':'1'}, 1200);      
+                $(this).animate({
+                	"opacity": "1"
+                }, 1200);      
             }  
         }); 
 	}
-
-	//Hide back-to-top btn at first
-	$(".back-to-top").hide();
 	
 	$(window).scroll(function () {
-		//Fade in back-to-top btn
+		//Fade in back-to-top btn when scroll 200px down 
 		if ($(this).scrollTop() > 200) {
-			$('.back-to-top').fadeIn(1000);
+			$(".back-to-top").fadeIn(1000);
 		} else {
-			$('.back-to-top').fadeOut();
+			$(".back-to-top").fadeOut();
 		}
 
 		//Animate background position y on scroll
-		$('body').css("background-position-y", parseInt($(this).scrollTop()*0.1));
+		$("body").css("background-position-y", parseInt($(this).scrollTop()*-0.3));
 
-		//Fade div call back
+		//Fade in divs on scroll
 		fadeDiv();
-	});
-
-	//Scroll body to 0px on click
-	$('.back-to-top a').click(function () {
-		$('body, html').animate({
-			scrollTop: 0
-		}, 700);
-		return false;
 	});
 });
